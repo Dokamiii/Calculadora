2c9e7d793860047c3da5602237e0927512d167f4
Dokamiii <eduardofranciscoln110@gmail.com>
2025-03-04 23:09:56 -0300

    agora ele manter o resultado da equacao anterior.

--- Main.java
+++ Main.java
@@ -5,43 +5,52 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int resultado;
+        int resultado = 0;
         char continuar;
-        do { 
-            System.out.println("primeiro numero");
-            int valor1 = teclado.nextInt();
-            teclado.nextLine();
-    
+        char continuarResultado = 'n';
+        int valor1 = 0;
+        int valor2 = 0;
+        do {
+            if(continuarResultado == 'n'|| continuarResultado == 'N') {
+                System.out.println("primeiro numero");
+                valor1 = teclado.nextInt();
+                teclado.nextLine();
+            } else {
+                valor1 = resultado;
+            }
             System.out.println("Selecione o operador: (+, -, *, /)");
             String operador = teclado.nextLine();
-    
+            
             System.out.println("Segundo numero");
-            int valor2 = teclado.nextInt();
-    
+            valor2 = teclado.nextInt();
+            
             switch (operador) {
                 case "+":
-                    resultado = valor1 + valor2;
-                    System.out.println(resultado);
-                    break;
+                resultado = valor1 + valor2;
+                System.out.println(resultado);
+                break;
                 case "-":
-                    resultado = valor1 - valor2;
-                    System.out.println(resultado);
-                    break;
+                resultado = valor1 - valor2;
+                System.out.println(resultado);
+                break;
                 case "*":
-                    resultado = valor1 * valor2;
-                    System.out.println(resultado);
-                    break;
+                resultado = valor1 * valor2;
+                System.out.println(resultado);
+                break;
                 case "/":
-                    resultado = valor1 / valor2;
-                    if(valor2 == 0){
-                        System.out.println("Nao divisivel por zero");
-                    }
-                    System.out.println(resultado);
-                    break;
+                resultado = valor1 / valor2;
+                if(valor2 == 0){
+                    System.out.println("Nao divisivel por zero");
+                }
+                System.out.println(resultado);
+                break;
             }
-        
             System.out.println("Deseja continuar? S/N");
             continuar = teclado.next().charAt(0);
+            teclado.nextLine();
+            
+            System.out.println("Deseja manter o mesmo resultado? S/N");
+            continuarResultado = teclado.next().charAt(0);
             teclado.nextLine();
 
         } while (continuar == 's' || continuar =='S');
